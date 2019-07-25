0a6537d76560fd2bec28ca5db0027b67c46f8331
Tyler Sefcik <tyler.sefcik@gmail.com>
2019-07-24 22:52:52 -0400

    LR starting

---
+++ project/LogisticRegression.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+"""
+    This class performs everything related to Logistic Regression.
+"""
+
+
+class LogisticRegression:
+    def __init__(self):
+        self.class_weights = []
+
+    
+
